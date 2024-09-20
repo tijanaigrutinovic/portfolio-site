c87ed4e466538eb4d8c0357875a26c7ec08ceba3
tijanaigrutinovic <tijana.igrutinovic@gmail.com>
2024-09-21 00:06:08 +0200

    redesign portfolio WIP

--- src/js/custom-cursor.js
+++ src/js/custom-cursor.js
@@ -2,8 +2,8 @@ export function customCursor() {
     const cursor = document.querySelector('#cursor');
 
     document.addEventListener('mousemove', (event) => {
-        cursor.style.left = `${event.pageX}px`;
-        cursor.style.top = `${event.pageY}px`;
+        cursor.style.left = `${event.clientX}px`; // No need for scroll offset since we're using position: fixed
+        cursor.style.top = `${event.clientY}px`;
         cursor.style.opacity = '1'; 
     });
 
@@ -15,3 +15,4 @@ export function customCursor() {
         cursor.style.opacity = '0';
     });
 }
+
