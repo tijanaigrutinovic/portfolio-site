1a1dabc52a8350d04fe9a6c200ea12eb404e3720
tijanaigrutinovic <tijana.igrutinovic@gmail.com>
2024-09-25 17:32:46 +0200

    fixing responsive nav modal

--- src/js/hamburger-menu.js
+++ src/js/hamburger-menu.js
@@ -2,6 +2,7 @@ export function hamburgerMenu() {
     const hamburger = document.getElementById('hamburger');
     const modal = document.getElementById('modal');
     const closeModal = document.getElementById('close');
+    const navLinks = document.querySelectorAll('#modal a'); 
 
     hamburger.addEventListener('click', () => {
         modal.style.display = 'block'; 
@@ -15,5 +16,11 @@ export function hamburgerMenu() {
         if (event.target === modal) {
             modal.style.display = 'none';
         }
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
     });
 }
