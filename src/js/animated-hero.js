88544b53cdff530978152f2b3d192d97e46eb552
tijanaigrutinovic <tijana.igrutinovic@gmail.com>
2024-09-26 12:18:11 +0200

    slowing name animation

--- src/js/animated-hero.js
+++ src/js/animated-hero.js
@@ -1,31 +1,35 @@
 export function animatedHero() {
-    //animated name on hero
     const animatedText = document.querySelector('#animatedText');
-    const textToAnimate = 'Tijana Igrutinović'; 
+    const textToAnimate = 'Tijana Igrutinović';
     
     let index = 0;
     let isDeleting = false;
     let speed = 100;
-    
+    let pauseAfterTyping = 600; 
+
     function typeWriter() {
         if (!isDeleting) {
             if (index < textToAnimate.length) {
                 animatedText.textContent += textToAnimate.charAt(index);
                 index++;
             } else {
-                isDeleting = true;
-                speed = 50; 
+                setTimeout(() => {
+                    isDeleting = true;
+                    typeWriter(); 
+                }, pauseAfterTyping);
+                return;
             }
         } else {
             if (index > 0) {
                 animatedText.textContent = textToAnimate.substring(0, index - 1);
                 index--;
             } else {
-                isDeleting = false;
-                speed = 100; 
+                isDeleting = false; 
             }
         }
+
         setTimeout(typeWriter, speed);
     }
+
     typeWriter();
 }
